c54463ef9e863d383793df3fc9ed2abec320fba3
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2022-09-02 18:33:50 +0100

    sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,24 +1,25 @@
 export default {
     title: "Cosmos Genesis",
     themeConfig: {
-        sidebar: [
-            {
-                text: "Cosmos",
-                activeMatch: "/main/",
-                items: [
-                    { text: "Home Page", link: "/index" },
-                    { text: "Team", link: "/main/team" }
-                ]
-            },
-            {
-                text: "Mods",
-                activeMatch: "/mods/",
-                items: [
-                    { text: "Generator", link: "/mods/generator" },
-                    { text: "Biome", link: "/mods/biome" }
-                ]
-            }
-        ],
+        sidebar: {
+            "/main/": [
+                {
+                    text: "Introduction",
+                    items: [
+                        { text: "Our Team", link: "/main/team" }
+                    ]
+                }
+            ],
+            "/mods/": [
+                {
+                    "text": "Mods",
+                    items: [
+                        { text: "Generator", link: "/mods/generator" },
+                        { text: "Biome", link: "/mods/biome" }
+                    ]
+                }
+            ]
+        },
         footer: {
             message: 'Released under the GNU General Public License v3.0.',
             copyright: 'Copyright © 2022-present FrederoxDev'
@@ -36,7 +37,7 @@ export default {
             }
         ],
         editLink: {
-          pattern: "https://github.com/FrederoxDev/Frederox.com/edit/master/docs/:path"
+            pattern: "https://github.com/FrederoxDev/Frederox.com/edit/master/docs/:path"
         }
     }
 }
